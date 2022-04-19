54aafbb1b048d3ae10f164e07a5c0edf0f4fbe9f
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-04-19 15:13:11 -0300

    Component: Item

---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,12 @@
+import * as C from './styles' 
+import {Item} from '../../types/Item'
+
+type Props = {
+    item: Item
+}
+
+export const ListItem = ({item}: Props) => {
+    return (
+        <C.Container>{item.name}</C.Container>
+    );
+}
